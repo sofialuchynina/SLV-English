cb94acc83121c24c17b802a68608179bdb393eb3
sofialuchynina <sluchynin@gmail.com>
2026-02-24 19:27:26 +0200

    Add files via upload

--- animation.js
+++ animation.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    // Анімація для classes
     const lessonCards = document.querySelectorAll(".lesson-card");
     lessonCards.forEach((card, index) => {
         setTimeout(() => {
@@ -8,7 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
         }, index * 200);
     });
 
-    // Анімація для progress
     const progressCard = document.querySelector(".progress-card");
     if (progressCard) {
         setTimeout(() => {
@@ -27,22 +25,21 @@ document.addEventListener("DOMContentLoaded", () => {
     if (quiz) {
         setTimeout(() => {
             quiz.classList.add("show");
-        }, 300); // З'явиться через 0.3 сек
+        }, 300); 
     }
 
-    // Анімація для чат-бота
     const bot = document.querySelector(".bot-container");
     if (bot) {
         setTimeout(() => {
             bot.classList.add("show");
-        }, 600); // З'явиться через 0.6 сек (після квізу)
+        }, 600);
     }
 
     const messages = document.querySelectorAll(".msg");
     messages.forEach((msg, index) => {
         setTimeout(() => {
             msg.classList.add("show");
-        }, 1000 + (index * 400)); // Повідомлення випливають одне за одним
+        }, 1000 + (index * 400)); 
     });
 
 });
